468bea546318189284e85eaee72ed7db515c785c
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-28 22:59:53 +0200

    Create VectorsMatrices.R

---
+++ VectorsMatrices.R
@@ -0,0 +1,19 @@
+// Vector
+y = c(4,5,6)
+Y = y*10
+y + Y
+y[1]
+y[y<6]
+gender = c("male", "female")
+
+// Matrix
+mat = matrix(c(1,2,3,4), nrow=2, byrow=TRUE)
+mat[1,2]
+mat[2,]
+
+
+// Sequence
+2:7
+seq(from=1, to=7, by=1)
+rep(1, times=10)
+rep(1:3, times=5)
